5d55315af3caea5dd5b6558e552bc9a8fbc6f76c
dineshkharat206 <102207557+dineshkharat206@users.noreply.github.com>
2025-02-22 16:59:23 +0530

    Implementing rate limiter in account MS

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.entity.Customer;
 import com.eazybytes.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -109,10 +110,16 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body("0.9");
     }
 
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion()
     {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body("JAVA 17");
     }
 
     @GetMapping("/contact-info")
